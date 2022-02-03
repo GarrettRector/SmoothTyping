2910ba8e84e2d34c45c071f6badcdd67094a484a
Garrett Rector <garrett@grector.dev>
2022-02-03 12:03:59 -0500

    updated hello world

---
+++ examples/hello world.py
@@ -0,0 +1,4 @@
+from src.smoothtyping import smoothtype
+
+# uses the smoothtype class to type "Hello, World!" to the console with 0.2 seconds in between each character
+smoothtype("Hello, World!", speed=0.2)
